65ce115d6c66960d305657799a6410b4227c9ebd
allpower2slaves <allpower2slaves@noreply.github.com>
2024-08-07 14:38:31 +0300

    added fish configs and updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # i didnt test it on anything posix. it should work with gnu coreutils
 
-ln -isvn $(realpath ./mpv/) ~/.config/mpv
-ln -isvn $(realpath ./nvim/) ~/.config/nvim
+ln -isvn $(realpath ./mpv) ~/.config/mpv
+ln -isvn $(realpath ./nvim) ~/.config/nvim
+ln -isvn $(realpath ./fish) ~/.config/fish
 
 if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ] && [ $(command -v nvim && return 1) ] ; then
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
