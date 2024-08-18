0f865aad97ce40aecb1c7ad2206cea9f72334d4c
allpower2slaves <allpower2slaves@noreply.github.com>
2024-08-18 12:09:04 +0300

    added dconf file for gnome setup

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 ln -isvn $(realpath ./mpv) ~/.config/mpv
 ln -isvn $(realpath ./nvim) ~/.config/nvim
-ln -isvn $(realpath ./fish) ~/.config/fish
+rm -rv ~/.config/fish && ln -isvn $(realpath ./fish) ~/.config/fish
 
 if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ] && [ $(command -v nvim && return 1) ] ; then
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
