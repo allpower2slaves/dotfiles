6e739f07af48d5bae28c44d79b899239da54b3a9
allpower2slaves <allpower2slaves@noreply.github.com>
2024-08-18 12:26:56 +0300

    added dconf apply mechanism

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ] && [ $(command -v nvim &&
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
+
+cat ./gnome-dconf.dconf | dconf load /
