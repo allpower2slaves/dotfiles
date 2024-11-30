99a988322e0846dd52b7f195b507040fcbba6440
allpower2slaves <allpower2slaves@noreply.github.com>
2024-11-30 13:27:15 +0300

    changed install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 
 ln -isvn $(realpath ./mpv) ~/.config/mpv
 ln -isvn $(realpath ./nvim) ~/.config/nvim
-rm -rv ~/.config/fish && ln -isvn $(realpath ./fish) ~/.config/fish
+ln -isvn $(realpath ./fish) ~/.config/fish
 
 if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ] && [ $(command -v nvim && return 1) ] ; then
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
-cat ./gnome-dconf.dconf | dconf load /
+test "$XDG_CURRENT_DESKTOP" = "GNOME" && \
+	dconf load / < ./gnome-setup.dconf
