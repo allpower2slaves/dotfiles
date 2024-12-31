4a46b2620f1d4601c32dbfaf664aac8e9e7cdd51
allpower2slaves <allpower2slaves@noreply.github.com>
2024-12-31 21:58:20 +0300

    fix for nvim vim-plug installation

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 # i didnt test it on anything posix. it should work with gnu coreutils
 
 ln -isvn $(realpath ./mpv) ~/.config/mpv
 ln -isvn $(realpath ./nvim) ~/.config/nvim
 ln -isvn $(realpath ./fish) ~/.config/fish
 
-if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ] && [ $(command -v nvim && return 1) ] ; then
+test ! -e ~/.local/share/nvim/site/autoload/plug.vim  && command -v nvim >/dev/null && \
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-fi
 
 test "$XDG_CURRENT_DESKTOP" = "GNOME" && \
 	dconf load / < ./gnome-setup.dconf
