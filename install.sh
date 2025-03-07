ce1fd4e2edce214700c9a3786e1bdfc525792490
allpower2slaves <allpower2slaves@github.noreply>
2025-03-07 10:41:45 +0300

    commented out vim-plug installation and GNOME dconf loading

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ ln -isvn $(realpath ./nvim) ~/.config/nvim
 ln -isvn $(realpath ./fish) ~/.config/fish
 ln -isvn $(realpath ./ghostty) ~/.config/ghostty
 
-test ! -e ~/.local/share/nvim/site/autoload/plug.vim  && command -v nvim >/dev/null && \
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#test ! -e ~/.local/share/nvim/site/autoload/plug.vim  && command -v nvim >/dev/null && \
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-test "$XDG_CURRENT_DESKTOP" = "GNOME" && \
-	dconf load / < ./gnome-setup.dconf
+#test "$XDG_CURRENT_DESKTOP" = "GNOME" && \
+#	dconf load / < ./gnome-setup.dconf
