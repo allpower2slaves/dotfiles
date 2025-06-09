ceafa2410085dbfa6d3078a93b672d81ee0740f0
allpower2slaves <allpower2slaves@github.noreply>
2025-06-09 13:07:33 +0300

    removed vim-plug old cruft

--- install.sh
+++ install.sh
@@ -7,10 +7,3 @@ ln -isvn $(realpath ./fish) ~/.config/fish
 ln -isvn $(realpath ./ghostty) ~/.config/ghostty
 
 test ! -e ~/.zshrc && ln -iv $(realpath ./zsh/zshrc) ~/.zshrc 2>/dev/null
-
-#test ! -e ~/.local/share/nvim/site/autoload/plug.vim  && command -v nvim >/dev/null && \
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-#test "$XDG_CURRENT_DESKTOP" = "GNOME" && \
-#	dconf load / < ./gnome-setup.dconf
