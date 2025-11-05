32ed608806e33b7d7b4fc7a66143bac4f37a2f61
allpower2slaves <allpower2slaves@github.noreply>
2025-11-05 22:43:18 +0300

    (install.sh) reimplemented the whole script

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-# i didnt test it on anything posix. it should work with gnu coreutils
 
-classic_dirs="mpv nvim fish ghostty" # list of 
-xdg_config="$HOME/.config" # because this thing will be ran on BSD and other unixes
+xdg_config_dir="$HOME/.config" # because this thing will be ran on BSD and other unixes
 
 # ~/.config/ creation
-test -d $xdg_config || mkdir -p $xdg_config
+test -d "$xdg_config_dir" || mkdir -p "$xdg_config_dir"
 
-# classic dir linking TODO: maybe fix filenames with space? (i mean i just copy configs lol)
-printf "$classic_dirs\n" | tr ' ' '\n' | while read directory; do
-	target="$xdg_config/$directory"
-	printf "$target\n" #debug
-	ln -svif $(realpath $directory) $target 
+# the symlink function
+linkitem(){
+	if [ -L "$2" ] || [ ! -e "$2" ]; then 
+		rm -fv "$2"
+		ln -fsv "$(realpath $1)" "$(realpath "$2")" # isnt the second realpath reduntant tho
+	else
+		printf "%s\n" ""$2" is a not a symbolic link, skipping..." 1>&2
+	fi
+}
+
+# install stuff in ~/.config
+printf "%s " "nvim mpv" | tr ' ' '\n' | while read __src; do
+	linkitem "$__src" ""$xdg_config_dir"/"$__src""
 done
 
-# zsh 
-ln -svif $(realpath ./zsh/zshrc) $HOME/.zshrc
+# zsh, bash and nex/nvi
+printf "%s " "zshrc zshenv bashrc nexrc zprofile" | tr ' ' '\n' | while read __src; do
+	linkitem "$__src" ""$HOME"/".$__src""
+done
