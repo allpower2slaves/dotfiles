bc704d69bd64ce89c26ae96a37dbf2b98e39eab7
allpower2slaves <allpower2slaves@github.noreply>
2025-11-05 22:48:12 +0300

    (install.sh) removed -v from commands

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ test -d "$xdg_config_dir" || mkdir -p "$xdg_config_dir"
 # the symlink function
 linkitem(){
 	if [ -L "$2" ] || [ ! -e "$2" ]; then 
-		rm -fv "$2"
-		ln -fsv "$(realpath $1)" "$(realpath "$2")" # isnt the second realpath reduntant tho
+		rm -f "$2"
+		ln -fs "$(realpath $1)" "$(realpath "$2")" # isnt the second realpath reduntant tho
 	else
 		printf "%s\n" ""$2" is a not a symbolic link, skipping..." 1>&2
 	fi
